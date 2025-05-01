c6b2a7a89d1fa7d45c035866632ef5ceab1fe307
rodrigo-teixeira-silva <60649228+rodrigo-teixeira-silva@users.noreply.github.com>
2025-05-01 16:06:53 -0400

    finish aplication

--- industrial---platform/scripts/character_body_2d.gd
+++ industrial---platform/scripts/character_body_2d.gd
@@ -1,13 +1,18 @@
 extends CharacterBody2D
 
-#const speed = 400.0
 @export var SPEED = 3
-@export_range(0,1) 
+@export_range(0, 1) 
 var lerp_factory = 0.5
 
+@export_range(0, 90)
+var max_tilt_degrees := 45
 
-func  _physics_process(_delta) -> void:	
-	var direction = Input.get_vector("ui_left", "ui_right", "ui_up","ui_down" )
+func _physics_process(_delta) -> void:
+	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var target_velocity = direction * SPEED * 100.0
-	velocity = lerp(velocity, target_velocity, lerp_factory)	
-	move_and_slide()	
+	velocity = lerp(velocity, target_velocity, lerp_factory)
+	move_and_slide()
+
+	# Corrigido: agora inclina para o lado certo
+	var tilt = lerp(rotation_degrees, direction.x * max_tilt_degrees, lerp_factory)
+	rotation_degrees = tilt
