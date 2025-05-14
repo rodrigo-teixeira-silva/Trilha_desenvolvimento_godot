909a12e656c6a66fe367a38d1b407532ffe68b7e
rodrigo-teixeira-silva <60649228+rodrigo-teixeira-silva@users.noreply.github.com>
2025-05-14 16:53:05 -0400

    fead: created a move pawns and AI

--- lore-swords/código.md
+++ lore-swords/código.md
@@ -100,4 +100,3 @@ func attack() -> void:
 	is_attacking = true	
 			
 			
-
--- lore-swords/enemies/behavior/follow.gd
+++ lore-swords/enemies/behavior/follow.gd
@@ -1,11 +1,21 @@
-extends Node
+extends CharacterBody2D
 
+@export var speed = 1
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 
+func _physics_process(delta: float) -> void:
+	var player_position = GameManager.player_position
+	var diference = player_position - position
+	var input_vector = diference.normalized()
+										   
+#input vector = vector =vector2 entre -1 e 1 em ambos os eixos
+	velocity = input_vector * speed * 100.0
+	move_and_slide()
+	
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+		# Girar o sprite 	
+	if input_vector.x > 0:
+		sprite.flip_h = false
+	elif input_vector.x < 0:		
+		sprite.flip_h = true		
