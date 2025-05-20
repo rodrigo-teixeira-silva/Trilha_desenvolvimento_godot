26c5b1f02c34d150d558539881e5b69928756c5b
rodrigo-teixeira-silva <60649228+rodrigo-teixeira-silva@users.noreply.github.com>
2025-05-20 16:42:26 -0400

    feat:  definition function death and color impact

--- lore-swords/enemies/enemy.gd
+++ lore-swords/enemies/enemy.gd
@@ -2,7 +2,28 @@ class_name Enemy
 extends Node2D
 
 @export var health: int = 10
+@export var death_prefab: PackedScene
  
 func damage(amount: int) -> void:
 	health -= amount
 	print("inimigo atingido",amount, ". Avida total é", health )
+	
+	#Receber dano
+	modulate = Color.RED
+	var tween = create_tween()
+	tween.set_ease(Tween.EASE_IN)
+	tween.set_trans(Tween.TRANS_QUINT)
+	tween.tween_property(self, "modulate", Color.WHITE, 0.3)
+	
+	if health <= 0:
+		die()
+		
+func die() -> void:
+	if death_prefab:
+		var death_object = death_prefab.instantiate()
+		death_object.position = position
+		get_parent().add_child(death_object)
+			
+	queue_free()		
+		
+		
