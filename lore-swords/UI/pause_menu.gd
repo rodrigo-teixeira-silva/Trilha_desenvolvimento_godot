935c526b79942dd4f7942e5d291f509a760e7ada
rodrigo-teixeira-silva <60649228+rodrigo-teixeira-silva@users.noreply.github.com>
2025-06-02 16:54:16 -0400

    tela_de_pause_final
    Conclusão da tela de pause, utilizando teclas do teclado (esc, setas, Enter) e  mouse. Inicio da estrutura da HUD

--- lore-swords/UI/pause_menu.gd
+++ lore-swords/UI/pause_menu.gd
@@ -1,4 +1,6 @@
 extends CanvasLayer
+@onready var resume: Button = $menu_holder/resume
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -6,21 +8,24 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
 
 
 func _on_resume_pressed() -> void:
-	pass # Replace with function body.
-
-
+	get_tree().paused = false
+	visible = false
 
 
 func _on_quit_game_pressed() -> void:
 	get_tree().quit()
-
+	
 
 func _unhandled_input(event):
 	if event.is_action_pressed("ui_cancel"):
 		visible = true
 		get_tree().paused = true
+		resume.grab_focus()
+
+
+		
